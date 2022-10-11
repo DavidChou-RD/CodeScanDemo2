c6a6d2497743dc2be32548ea7d76a384c371d614
DavidChou-RD <davidchou@gorilla-technology.com>
2022-10-11 17:52:59 +0800

    [what] Add main function & csproj & .gitignore

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SetTowResult();
+    }
+
+    public static void SetTowResult()
+    {
+        var arr = new int[] { 2, 17, 7, 15 };
+        var result = TwoSum(arr, 9);
+        Console.WriteLine($"Two sum of 9 is {arr[result[0]]} and {arr[result[1]]}. Indexes are {result[0]} and {result[1]}");
+    }
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(target - nums[i]))
+            {
+                return new int[] { dict[target - nums[i]], i };
+            }
+            else
+            {
+                dict[nums[i]] = i;
+            }
+        }
+        return new int[] { };
+    }
+}
